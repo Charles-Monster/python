a24975689777326f46e685b8377c6c6821cf20c4
Charles-Monster <charlesweng1011@gmail.com>
2023-06-30 21:45:28 +0800

    hw3

--- class3.py/hw3.py/hw3.py
+++ class3.py/hw3.py/hw3.py
@@ -1,10 +1,15 @@
 1. 請問使用什麼function可以顯示出形態
-答案:
+答案:type
 2.請問今天學了哪一些function(函式)?
-答案:
+答案:int print type float str bool
 3. 延續上題, 請嘗試描述每個function的功能各別是什麼?
-答案:
+答案:type可以顯示()裡的資料
+int可以把()裡的數直變成整數
+float可以把()裡的資料變成浮點數
+bool可以把()裡的數值變成布林
+str可以把()裡的數值變成字串
 4. 請使用者輸入自己的年齡(input)
 程式會顯示出(print)
 你的年齡是
-XX
+xx
+10
