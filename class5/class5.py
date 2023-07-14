c8efab2fe444bd111de0363da0b4cbe6a328f3c4
Charles-Monster <charlesweng1011@gmail.com>
2023-07-14 21:22:10 +0800

    hw5

---
+++ class5/class5.py
@@ -0,0 +1,13 @@
+# name = 'python'
+# age = 31
+# print("我叫做" + name + "我今年" + str(age) + "歲了")
+
+#  %S代表字串的變數
+#  %f代表小數的變數
+#  %d代表整數的變數
+
+# print("我是%s,我今年%d歲了!" % (name, age))
+name = 'python'
+age = input("請輸入python年齡:")
+age = int(age)
+print("我叫做" + name + "今年" + str(age) + "十年後是 "+)
