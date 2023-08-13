dccdb3e71841bdcaf6650c599662e7803fa9b350
Charles-Monster <charlesweng1011@gmail.com>
2023-08-13 11:39:15 +0800

    hw8

--- class8/hw8.py
+++ class8/hw8.py
@@ -20,3 +20,14 @@ EX:
 你的BMI為31.14186851211073
 體重過重
 '''
+h = float(input("請輸入身高(公尺):"))
+w = float(input("請輸入體重(公斤):"))
+bmi = w / h**2
+print("你的bmi為" + str(bmi))
+
+if bmi >= 14.8 and bmi <= 20.7:
+    print("體重正常")
+elif bmi > 20.7:
+    print("體重過重")
+else:
+    print("體重過輕")
