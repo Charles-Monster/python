52e8412825d170bdf2480a042ba6d7009fc0b975
Charles-Monster <charlesweng1011@gmail.com>
2023-08-18 20:54:08 +0800

    class9+hw

---
+++ class9/class9.py
@@ -0,0 +1,27 @@
+import turtle
+# MOVE TURTLE
+#turtle.forward(150)  #int or float
+#turtle.left(90)  #int or float
+#turtle.forward(150)  #int or float
+#turtle.left(90)  #int or float
+#turtle.forward(150)  #int or float
+#turtle.left(90)  #int or float
+#turtle.done()  # 讓視窗保持顯示
+# turtle.forward(60)  #int or float
+# turtle.left(60)  #int or float
+# turtle.forward(60)  #int or float
+# turtle.left(60)  #int or float
+# turtle.forward(60)  #int or float
+# turtle.left(60)  #int or float
+# turtle.forward(60)  #int or float
+# turtle.left(60)  #int or float
+# turtle.forward(60)  #int or float
+# turtle.left(60)  #int or float
+# turtle.forward(60)  #int or float
+# turtle.left(60)  #int or float
+# turtle.done()  # 讓視窗保持顯示
+for i in range(2, 10, 2):
+    print(i)
+# for 迴圈變數 in range(執行次數(起始質,中止質,變化質)):
+#     要重複的程式
+#hello hello hello hello hello
