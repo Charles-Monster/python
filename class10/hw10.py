74cdb7741138f9c06cfd319fc6cf0f485ffea44f
Charles-Monster <charlesweng1011@gmail.com>
2023-09-01 19:41:05 +0800

    class10+hw10

---
+++ class10/hw10.py
@@ -0,0 +1,11 @@
+#turtle.home() # 是讓烏龜回到原點的指令
+#turtle.clear() # 清空畫面指令
+#import time  # 匯入time模組
+#time.sleep(1) # 延遲1秒
+import turtle
+for i in range(8):
+    turtle.stamp()
+    turtle.forward(100)
+    turtle.stamp()
+
+    turtle.home()
