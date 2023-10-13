009df3d3778f6e37c35be67da3b7825aa44f994d
Charles-Monster <charlesweng1011@gmail.com>
2023-10-13 21:05:50 +0800

    class15+hw

---
+++ class15/class15.py
@@ -0,0 +1,33 @@
+# random.randrange(終止值)
+# random.randrange(起始值, 終止值, 遞進值)
+# import random
+
+# random.randint(1, 3)
+# random.randint(1, 10)
+# list
+# a = ['蘋果', '香蕉', '葡萄']
+# print(a[2])
+# L = [1, 2, 3, 'Hello', 'World']
+# print(L)
+#LIST裡面可以加入LIST
+# L = [1, 2, 3, 'Hello', 'World', [4, 5, 6]]
+# print(L[5][1])
+#LIST也可以到著數
+# L = [0, 1, 2, 3, 4, 5, 6]
+# L[0:5:2]
+# L[::3]
+# print(L[-1])
+#LEN可以取得LIST的長度(len若要顯示要在終端機打)
+# len([])
+# len(['蘋果'])
+# len(['a', 'b'])
+# len([1, 2, 3])
+# print(len)
+#INDEX=索引值
+# L = ['a', 'b', 'c']
+# for index in range(len(L)):
+#     print(L[index])
+#ELEMENT(走訪元素)是依照順序讀取每個元素值
+# L = ['a', 'b', 'c']
+# for element in L:
+#     print(element)
