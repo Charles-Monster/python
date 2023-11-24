126a8a94a5f2d11334460b3440989baa5cc4688f
Charles-Monster <charlesweng1011@gmail.com>
2023-11-24 21:05:25 +0800

    class21+hw

---
+++ class21/class21.py
@@ -0,0 +1,16 @@
+ # def hello():
+# #def代表宣告一個函數
+#     print('hello')
+# hello()
+# apple=['d','f']
+# print(len(apple))
+# def hello(name):
+#     print('hello'+str(name))
+# hello('仕奇')
+# def my_min(a,b):
+#     if a<b:
+#         return a
+#     else:
+#         return b
+
+# print(my_min(1,2)) 
