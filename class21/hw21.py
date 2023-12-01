300a195bd562665026c5af0eb0f6820425590874
Charles-Monster <charlesweng1011@gmail.com>
2023-12-01 19:58:39 +0800

    class21+hw

--- class21/hw21.py
+++ class21/hw21.py
@@ -6,11 +6,22 @@
 import random
 def roll_dice(n):
 '''
+#上面是我的作業
+n=(int(input('請輸入擲骰子的次數:')))
+for n in range(n):
+    import random
+    ans=random.randint(1,6)
+    def roll_dice(n):
+        print(str(ans))
+    roll_dice(n)
+#下面是老師的範例
 import random
-ans=(int(input('請輸入擲骰子的次數:')))
-random.randrange(ans)
-def roll_dice():
-    print(str(random))
-roll_dice()
 
-    
+def roll_dice(n):
+    dice=[]
+    for i in range(n):
+        dice.append(random.randint(1,6))
+    #回傳結果到dice，dice會新增迴圈中的隨機數
+    return dice
+cnt=int(input('輸入丟骰子的次數:'))
+print(roll_dice(cnt))
